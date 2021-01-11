d63e95e9354500f85d5a56bdf0d25c410ce008d8
umbreonight <darrenvanreenen@live.com>
2021-01-11 17:27:56 +0200

    rehash form

--- components/input.js
+++ components/input.js
@@ -3,16 +3,12 @@ import styles from "./input.module.scss"
 
 export default function Input({ type, name, id, placeholder, required, label, helper }) {
 
-    const [value, setValue] = useState()
-
     return (
         <div className={styles.input}>
             <input
                 type={type}
                 name={name}
                 id={id}
-                value={value || ""}
-                onChange={(event) => { setValue(event.target.value) }}
                 placeholder={placeholder}
                 required={required ? true : false}
             ></input>
