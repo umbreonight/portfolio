caeab181a49d0efcbac1b48e6cadf629b6f271dc
umbreonight <darrenvanreenen@live.com>
2021-01-11 12:36:55 +0200

    Updated Netliy Server settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = {
+    // Target must be serverless
+    target: 'serverless',
+
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
         includePaths: ['./src'],
